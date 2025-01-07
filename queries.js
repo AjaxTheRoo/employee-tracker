8cda001c1c609e7de5f34a5517030c05178a9ed7
AjaxTheRoo <86646399+AjaxTheRoo@users.noreply.github.com>
2025-01-06 17:27:04 -0800

    Update queries.js

--- queries.js
+++ queries.js
@@ -1,4 +1,4 @@
-const db = require('./connection');
+const db = require('./db/connection');
 
 const viewAllDepartments = () => {
     return db.query('SELECT * FROM department');
@@ -9,23 +9,23 @@ const viewAllRoles = () => {
 };
 
 const viewAllEmployees = () => {
-    return db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id');
-}; 
+    return db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, \' \', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id');
+};
 
 const addDepartment = (name) => {
     return db.query('INSERT INTO department (name) VALUES ($1)', [name]);
 };
 
-const addRole = (title, salary, departmentId) => {
-    return db.query('INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)', [title, salary, departmentId]);
+const addRole = (title, salary, department_id) => {
+    return db.query('INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)', [title, salary, department_id]);
 };
 
-const addEmployee = (firstName, lastName, roleId, managerId) => {
-    return db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)', [firstName, lastName, roleId, managerId]);
+const addEmployee = (first_name, last_name, role_id, manager_id) => {
+    return db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)', [first_name, last_name, role_id, manager_id]);
 };
 
-const updateEmployeeRole = (employeeId, roleId) => {
-    return db.query('UPDATE employee SET role_id = $1 WHERE id = $2', [roleId, employeeId]);
+const updateEmployeeRole = (employee_id, role_id) => {
+    return db.query('UPDATE employee SET role_id = $1 WHERE id = $2', [role_id, employee_id]);
 };
 
 module.exports = {
@@ -35,6 +35,5 @@ module.exports = {
     addDepartment,
     addRole,
     addEmployee,
-    updateEmployeeRole,
-    updateEmployeeManager
+    updateEmployeeRole
 };
