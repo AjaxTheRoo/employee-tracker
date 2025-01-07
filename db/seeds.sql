6c9e35473a3297458082107bd2c98ddda454dad9
AjaxTheRoo <86646399+AjaxTheRoo@users.noreply.github.com>
2025-01-06 17:20:58 -0800

    Update seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO department (name) VALUES ('Programing'), ('Design'), ('Finance'), ('Human Resources'), ('Marketing');
+INSERT INTO role (title, salary, department_id) VALUES ('Programmer', 60000, 1), ('Designer', 50000, 2), ('Accountant', 70000, 3), ('HR', 40000, 4), ('Marketer', 45000, 5);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Bart', 'Higgins', 1), ('Claire', 'Dubois', 2), ('Jasper', 'Vance', 3), ('Willow', 'Reed', 4), ('Orion', 'Stone', 5);
